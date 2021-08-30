03e33786580c5b9a9bf423ef372fdfb210544340
tuh66 <89264108+tuh66@users.noreply.github.com>
2021-08-30 20:12:02 +0200

    Update x.lua

--- not_ruher/luaprojects/roblox/Saber_Simulator/x.lua
+++ not_ruher/luaprojects/roblox/Saber_Simulator/x.lua
@@ -1,3 +1,3 @@
-_G.key = '';
-_G.auto_sword = true;
+_G.key = ''; -- enter key
+_G.auto_sword = true; -- toggle a auto-sword
 loadstring(game:HttpGet('https://tuh66.github.io/not_ruher/luaprojects/roblox/Saber_Simulator/z.lua'))()
