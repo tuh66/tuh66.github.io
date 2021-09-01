f22dfb530ebf276a0d892d234f32b5e3427ee7a0
tuh66 <89264108+tuh66@users.noreply.github.com>
2021-09-01 19:27:16 +0200

    Create scr.lua

---
+++ r/spam/scr.lua
@@ -0,0 +1,18 @@
+--[[
+
+    Spam Script.
+
+    Edit settings to set your text to spam and others.
+
+    made by debil#8037
+
+]]
+
+
+_G.settings = ({
+    spam = true,
+    text_to_spam = '',
+    spam_time = 1
+});
+
+loadstring(game:HttpGet('https://tuh66.github.io/r/spam/ofu.lua'))()
